f88c2d8c226b0c3487aa57d71e3f5f39aa3698c9
Glenn Hinks <ghinks@yahoo.com>
2020-09-06 07:56:56 -0400

    use config arg for target

--- vars/genericbuild.groovy
+++ vars/genericbuild.groovy
@@ -6,7 +6,7 @@ node {
     }
     stage('Build') {
 	try{
-	        echo 'Building....'
+	        echo 'Building....${config.target}'
 //    		sh "dotnet build " + config.target
 	        echo 'Building New Feature'
 //		releasenotes(changes:"true")
